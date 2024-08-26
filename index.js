c57472199a35d2c634f7aa8a578ac3e687370bd4
DuongNB <duongnb>
2024-08-26 08:32:33 +0700

    create cer on mac duongnb and update logic

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 const express = require("express");
+const path = require("path");
+const https = require("https");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
+// Load the self-signed certificate and private key
+const key = fs.readFileSync(path.join(__dirname, "cer/private-key.pem"));
+const cert = fs.readFileSync(path.join(__dirname, "cer/certificate.pem"));
+
+const options = {
+  key: key,
+  cert: cert,
+  requestCert: true,
+  rejectUnauthorized: false, // Set to false for self-signed certificates
+};
+
 app.get("/data", (req, res) => {
-  const forwarded = req.headers["x-forwarded-for"];
-  const ip = forwarded ? forwarded.split(/, /)[0] : req.ip;
+  const cert = req.socket.getPeerCertificate();
 
   let clientCertCN = "N/A";
   let clientCertIssuer = "N/A";
+  console.log(cert);
+  if (cert && cert.subject && cert.issuer) {
+    clientCertCN = cert.subject.CN || "N/A";
+    clientCertIssuer = cert.issuer.CN || "N/A";
+  } else {
+    const data = {
+      accessTime: "NA",
+      sourceIpAddress: "NA",
+      userAgent: "NA",
+      method: "NA",
+      statusCode: "NA",
+      clientCertCN: "NA",
+      clientCertIssuer: "NA",
+    };
 
-  if (req.secure && req.socket.getPeerCertificate) {
-    const cert = req.socket.getPeerCertificate();
-    if (cert && cert.subject && cert.issuer) {
-      clientCertCN = cert.subject.CN || "N/A";
-      clientCertIssuer = cert.issuer.CN || "N/A";
-    }
+    return res.json(data);
   }
+
+  const forwarded = req.headers["x-forwarded-for"];
+  const ip = forwarded ? forwarded.split(/, /)[0] : req.ip;
 
   const data = {
     accessTime: new Date().toISOString(),
@@ -30,7 +55,34 @@ app.get("/data", (req, res) => {
   };
   res.json(data);
 });
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+// Define the route to download the file
+app.get("/download", async (req, res) => {
+  const filePath = path.join(__dirname, "cer", "Certificate.p12");
+  res.download(filePath, "Certificate.p12", (err) => {
+    if (err) {
+      console.error("Error downloading the file:", err);
+      res.status(500).send("Error downloading the file");
+    } else {
+      console.log("File downloaded successfully");
+    }
+  });
 });
+https.createServer(options, app).listen(port, () => {
+  console.log(`Server is running at https://localhost:${port}`);
+});
+
+// # Generate a private key
+// openssl genrsa -out private-key.pem 2048
+
+// # Create a certificate signing request (CSR)
+// openssl req -new -key private-key.pem -out cert-request.csr
+
+// # Generate a self-signed certificate
+// openssl x509 -req -days 365 -in cert-request.csr -signkey private-key.pem -out certificate.pem
+
+// # Export the key and certificate to a .p12 file
+// openssl pkcs12 -export -legacy -out Certificate.p12 -in certificate.pem -inkey private-key.pem
+
+// openssl pkcs12 -export -legacy -out Certificate.p12 -in certificate.pem -inkey private-key.pem
+
+//openssl x509 -req -days 365 -in certificate.csr -signkey private-key.pem -out certificate.pem
